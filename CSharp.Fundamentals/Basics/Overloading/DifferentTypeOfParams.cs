aa295e5b407ed098ed0a63008cce2682935a80ae
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-10-04 20:56:17 +0800

    Added DifferentTypeOfParams - Overloading

---
+++ CSharp.Fundamentals/Basics/Overloading/DifferentTypeOfParams.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.Basics.Overloading
+{
+    /// <summary>
+    /// In the example below, both methods expect a single argument. So, based on the type of the argument passed during the method call,
+    /// the compiler binds the appropriate method definition.
+    /// </summary>
+    public class DifferentTypeOfParams
+    {
+        public static void Main(string[] args)
+        {
+            Create("Create!!!");
+        }
+
+        public static void Create(string a)
+        {
+            Console.WriteLine("String Create.");
+        }
+
+        public static void Create(int b)
+        {
+            Console.WriteLine("Integer Create.");
+        }
+    }
+
+    /*
+     Now, let us give the compiler something to think. We would overload a double and a float type method. 
+     We know that an integer can always be implicitly converted into a float type as well as a double type.
+
+     When we pass an integer argument, the compiler checks for implicit conversion and finds that the best possible conversion is an integer to float. 
+     Hence, the float method is called.
+     */
+}
