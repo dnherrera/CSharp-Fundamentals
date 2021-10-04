38d37d0f20f9b1c65a7db7a84cf6e161a12f80c4
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-10-04 21:13:20 +0800

    NamedArguments Overloading

---
+++ CSharp.Fundamentals/Basics/Overloading/NamedArguments.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.Basics.Overloading
+{
+    /// <summary>
+    /// C# has another feature of passing in the name of the arguments while calling the method. 
+    /// This helps in method overloading as well. 
+    /// Developers can choose to call a particular method even if the argument passed would have by default called another method. 
+    /// Although, the overloaded methods must differ in the signature.
+    /// </summary>
+    public class NamedArguments
+    {
+        public static void Main(string[] args)
+        {
+            Add(b: 10);
+        }
+
+        public static void Add(int a)
+        {
+            Console.WriteLine("Integer Param");
+        }
+
+        public static void Add(double b)
+        {
+            Console.WriteLine("Double Param"); // Result
+        }
+    }
+}
