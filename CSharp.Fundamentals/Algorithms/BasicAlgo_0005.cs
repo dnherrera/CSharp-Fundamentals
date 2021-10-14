d69e676513c2e5e15902c17278c9fe47bf4909f5
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-10-14 22:17:59 +0800

    BA_0005

---
+++ CSharp.Fundamentals/Algorithms/BasicAlgo_0005.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.Algorithms
+{
+    /// <summary>
+    /// Write a C# Sharp program to create a new string where 'if' is added to the front of a given string. 
+    /// If the string already begins with 'if', return the string unchanged
+    /// </summary>
+    public class BasicAlgo_0005
+    {
+        static void Main(string[] args)
+        {
+            string[] testData = { "if else", "else" };
+
+            for (int i = 0; i < testData.Length; i++)
+            {
+                Console.WriteLine(StringChecker(testData[i]));
+            }
+        }
+
+        static string StringChecker(string oldString)
+        {
+            if (oldString.StartsWith("if"))
+                return oldString;
+
+            return $"if { oldString }";
+        }
+    }
+}
