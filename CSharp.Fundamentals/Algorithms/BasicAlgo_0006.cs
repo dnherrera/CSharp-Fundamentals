7ebcbc7e64587344b37fef81e0385c2bab9b628b
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-10-14 22:57:16 +0800

    BA 0006

---
+++ CSharp.Fundamentals/Algorithms/BasicAlgo_0006.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp.Fundamentals.Algorithms
+{
+    /// <summary>
+    /// Write a C# Sharp program to remove the character in a given position of a given string. The given position will be in the range 0...string length -1 inclusive.
+    /// </summary>
+    public class BasicAlgo_0006
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<string, int> testData = new Dictionary<string, int>()
+            {
+                { "Dondon", 1 },
+                { "Philippines", 3 },
+                { "Canada", 4 }
+            };
+
+            foreach (var item in testData)
+            {
+                Console.WriteLine(StringTrimmer(item.Key, item.Value));
+            }
+        }
+
+        static string StringTrimmer(string request, int startIndex)
+        {
+            var numberOfCharTobeDeleted = 1;
+            var test = request.Remove(startIndex, numberOfCharTobeDeleted);
+            return test;
+        }
+    }
+}
