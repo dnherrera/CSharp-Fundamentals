7902b3f88e869e61ab444c297c9d4642bed4adb2
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-10-16 23:56:34 +0800

    Used substring to get the 2 characters of a string

---
+++ CSharp.Fundamentals/Algorithms/BasicAlgo_0008.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.Algorithms
+{
+    /// <summary>
+    /// Write a C# Sharp program to create a new string which is 4 copies of the 2 front characters of a given string. 
+    /// If the given string length is less than 2 return the original string.
+    /// </summary>
+    public class BasicAlgo_0008
+    {
+        static void Main(string[] args)
+        {
+            string[] testData = { "C Sharp", "JS", "b" };
+            foreach (var item in testData)
+            {
+                Console.WriteLine(CreateNewString(item));
+            }
+        }
+
+        public static string CreateNewString(string request)
+        {
+            return request.Length < 2 ? request : request.Substring(0, 2) + request.Substring(0, 2) + request.Substring(0, 2) + request.Substring(0, 2);
+        }
+    }
+}
