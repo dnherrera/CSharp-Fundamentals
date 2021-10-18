1bc1aeecc12cf547271320bc6d90009fbf6fcfa6
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-10-18 17:26:22 +0800

    AddFirstThreeCharacterToFrontAndBack

---
+++ CSharp.Fundamentals/Algorithms/BasicAlgo_0011.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.Algorithms
+{
+    /// <summary>
+    /// Write a C# Sharp program to create a new string taking the first 3 characters of a given string and return the string with the 3 characters added at both the front and back. 
+    /// If the given string length is less than 3, use whatever characters are there.
+    /// </summary>
+    public class BasicAlgo_0011
+    {
+        static void Main(string[] args)
+        {
+            string[] testData = { "Python", "JS", "Code" };
+            foreach (var item in testData)
+            {
+                Console.WriteLine(AddFirstThreeCharacterToFrontAndBack(item));
+            }
+        }
+
+        static string AddFirstThreeCharacterToFrontAndBack(string request)
+        {
+            return request.Length < 3 ? $"{request}{request}{request}" : $"{request.Substring(0, 3)}{request}{request.Substring(0, 3)}";
+        }
+    }
+}
