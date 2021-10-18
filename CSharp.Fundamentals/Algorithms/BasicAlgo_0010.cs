6c05c1cf9cef64ec9c755a9e413ba5bd14a056d8
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-10-18 17:10:04 +0800

    Multiple By 3 or 7

---
+++ CSharp.Fundamentals/Algorithms/BasicAlgo_0010.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.Algorithms
+{
+    /// <summary>
+    /// Write a C# Sharp program to check whether a given positive number is a multiple of 3 or a multiple of 7.
+    /// </summary>
+    public class BasicAlgo_0010
+    {
+        static void Main(string[] args)
+        {
+            int[] testData = { 3, 14, 12, 37 };
+            foreach (var item in testData)
+            {
+                Console.WriteLine(IsMultipleByThreeOrSeven(item));
+            }
+        }
+
+        static bool IsMultipleByThreeOrSeven(int request)
+        {
+            return request % 3 == 0 || request % 7 == 0;
+        }
+    }
+}
