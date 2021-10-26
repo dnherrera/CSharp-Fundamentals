a2c7e2293f7bd404ffa3c009c766b9467be64749
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-10-26 10:25:31 +0800

    To check which number is nearest to the value 100 among two given integers.

---
+++ CSharp.Fundamentals/Algorithms/BasicAlgo_0019.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp.Fundamentals.Algorithms
+{
+    /// <summary>
+    /// Write a C# Sharp program to check which number is nearest to the value 100 among two given integers. 
+    /// Return 0 if the two numbers are equal.
+    /// Sample Input:
+    /// 78, 95
+    /// 95, 95
+    /// 99, 70
+    /// Expected Output:
+    /// 95
+    /// 0
+    /// 99
+    /// </summary>
+    public class BasicAlgo_0019
+    {
+        public static void Main(string[] args)
+        {
+            IDictionary<int, int> testData = new Dictionary<int, int>
+            {
+                {75, 95},
+                {95, 95},
+                {99, 70},
+                {101, 99}
+            };
+
+            foreach (var item in testData)
+            {
+                Console.WriteLine(NearestValueChecker(item.Key, item.Value));
+            }
+        }
+
+        public static int NearestValueChecker(int x, int y)
+        {
+            const int def = 100;
+            var result = Math.Abs(x - def) < Math.Abs(y - def) ? x : y;
+            return x == y ? 0 : result;
+        }
+    }
+}
