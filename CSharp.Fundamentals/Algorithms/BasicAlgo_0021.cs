ff3e48dd09f70f4347acaa3663abbde85c21e6ee
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-10-26 21:26:40 +0800

    To find the larger value from two positive integer values that is in the range 20..30 inclusive, or return 0 if neither is in that range.

---
+++ CSharp.Fundamentals/Algorithms/BasicAlgo_0021.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Fundamentals.Algorithms
+{
+    /*
+     Write a C# Sharp program to find the larger value from two positive integer values that is in the range 20..30 inclusive, 
+    or return 0 if neither is in that range. Go to the editor
+     Sample Input:
+     78, 95
+     20, 30
+     21, 25
+     28, 28
+     Expected Output:
+     0
+     30
+     25
+     28
+     */
+    public class BasicAlgo_0021
+    {
+        public static void Main(string[] args)
+        {
+            var testData = new Dictionary<int, int>()
+            {
+                { 78, 95 },
+                { 20, 30 },
+                { 21, 25 },
+                { 28, 28 }
+            };
+
+            foreach (var item in testData)
+            {
+                Console.WriteLine(FindLargerValueInTheRange(item.Key, item.Value));
+            }
+        }
+
+        public static int FindLargerValueInTheRange(int x, int y)
+        {
+            var num = x > y ? x : y;
+            var res = num >= 20 && num <= 30 ? num : 0;
+            return res;
+        }
+    }
+}
