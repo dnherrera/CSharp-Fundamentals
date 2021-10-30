3313528da9f9f3bdde90d0ae18bbae417eaca70c
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-10-30 16:43:18 +0800

    Count the "aaa" or "aa" in the string

---
+++ CSharp.Fundamentals/Algorithms/BasicAlgo_0027.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.Algorithms
+{
+    /*
+     Write a C# Sharp program to count the string "aa" in a given string and assume "aaa" contains two "aa". Go to the editor
+     Sample Input:
+     "bbaaccaag"
+     "jjkiaaasew"
+     "JSaaakoiaaa"
+     Expected Output:
+     3
+     2
+     4
+     */
+    public class BasicAlgo_0027
+    {
+        public static void Main(string[] args)
+        {
+            var testData = new string[]
+            {
+                "bbaaaccgaa",
+                "jjkiaaasew",
+                "JSaaakoiaaa"
+            };
+
+            foreach (var item in testData)
+            {
+                Console.WriteLine(StringCounter(item));
+            }
+        }
+
+        public static int StringCounter(string request)
+        {
+            int ctr_aa = 0;
+            for (int i = 0; i < request.Length - 1; i++)
+            {
+                if (request.Substring(i, 2) == "aa")
+                {
+                    ctr_aa++;
+                }
+            }
+            return ctr_aa;
+        }
+    }
+}
