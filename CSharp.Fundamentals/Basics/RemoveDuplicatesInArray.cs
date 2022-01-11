828ce53bee893bbba4009648b252a125c60a2737
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-01-11 21:10:29 +0800

    Remove Duplicates in Two Arrays

---
+++ CSharp.Fundamentals/Basics/RemoveDuplicatesInArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp.Fundamentals.Basics
+{
+    public class RemoveDuplicatesInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 4, 4, 3 };
+            int[] arr2 = { 1, 2, 5, 4, 3 };
+
+            Console.WriteLine(string.Join(", ", arr1.Concat(arr2).Distinct()));
+        }
+    }
+}
