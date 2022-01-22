2c53c53494e24295c0edb616db5ac51373d849aa
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-01-22 23:45:46 +0800

    Sort Function Part 1

---
+++ CSharp.Fundamentals/Basics/SortFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Fundamentals.Basics
+{
+    class SortFunction
+    {
+        static void Main(string[] args)
+        {
+            var testData = new List<dynamic[]>
+            {
+                new dynamic[]{"aa", "ccc", "bb"}
+                //new dynamic[]{1, "ccc", "bb"}, need to implement this
+            };
+
+            foreach (var item in testData)
+            {
+                Console.WriteLine(string.Join(",", SortFunctions(item)));
+            }
+        }
+
+        static dynamic[] SortFunctions(dynamic[] args)
+        {
+            var test = args.OrderBy(x => x.Length).ToArray();
+            return test;
+        }
+    }
+}
