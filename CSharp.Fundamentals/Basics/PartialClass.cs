fec695a7fe17c1afdc8271743727d41c83ee68eb
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-01-23 23:19:35 +0800

    Partial Class 1

---
+++ CSharp.Fundamentals/Basics/PartialClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.Basics
+{
+    /// <summary>
+    ///  class whose definition is present in 2 or more files
+    /// </summary>
+    class PartialClass
+    {
+        public static void Main()
+        {
+            Student StudentObject = new Student();
+            StudentObject.Study();
+            StudentObject.Play();
+        }
+    }
+
+    public partial class Student
+    {
+        public void Study()
+        {
+            Console.WriteLine("I am studying");
+        }
+    }
+    public partial class Student
+    {
+        public void Play()
+        {
+            Console.WriteLine("I am Playing");
+        }
+    }
+}
