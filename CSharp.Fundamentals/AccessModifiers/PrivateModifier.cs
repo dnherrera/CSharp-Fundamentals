e80a135978d8307b44a07aff776e1dc0465a02d7
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-01-30 22:06:09 +0800

    Access Modifiers

---
+++ CSharp.Fundamentals/AccessModifiers/PrivateModifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.AccessModifiers
+{
+    public class PrivateModifier
+    {
+        static void Main(string[] args)
+        {
+            GetNumber num = new GetNumber();
+            //Console.WriteLine(num.number); // number variable can only accessible within its class.
+        }
+    }
+
+    public class GetNumber
+    {
+        private int number = 10;
+    }
+}
