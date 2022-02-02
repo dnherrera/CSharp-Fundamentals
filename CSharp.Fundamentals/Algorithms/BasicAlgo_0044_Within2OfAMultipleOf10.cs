6b5e477ac55a228e4757f2dd85d037694fbd4bca
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-02-02 21:42:15 +0800

    Within 2 of a Multiple of 10

---
+++ CSharp.Fundamentals/Algorithms/BasicAlgo_0044_Within2OfAMultipleOf10.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.Algorithms
+{
+    /// <summary>
+    /// Write a C# Sharp program to check whether a given number is within 2 of a multiple of 10.
+    /// </summary>
+    public class BasicAlgo_0044_Within2OfAMultipleOf10
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(3));
+            Console.WriteLine(test(7));
+            Console.WriteLine(test(8));
+            Console.WriteLine(test(21));
+            Console.ReadLine();
+        }
+        public static bool test(int n)
+        {
+            return n % 10 <= 2 || n % 10 >= 8;
+        }
+    }
+}
