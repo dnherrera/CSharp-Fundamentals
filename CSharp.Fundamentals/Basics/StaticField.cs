fda3c70541370ae0248c0f0bccca5a125fde318c
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-02-03 23:48:43 +0800

    Static Field

---
+++ CSharp.Fundamentals/Basics/StaticField.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp.Fundamentals.Basics
+{
+    public class StaticField
+    {
+        static void Main(string[] args)
+        {
+            Area A = new Area(); // A - is an instance of a class. 
+           // Console.WriteLine(A.PI); //This is because the PI is a static field. Static fields can only be accessed using the name of the class and not the instance of the class.
+        }
+    }
+
+    class Area
+    {
+        public static double PI = 3.14;
+    }
+}
