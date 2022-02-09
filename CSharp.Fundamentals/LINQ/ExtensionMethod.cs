cf27dd63f0cdf8458e71d0783c7cdd0f723043a1
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-02-09 21:48:44 +0800

    Extension Method

---
+++ CSharp.Fundamentals/LINQ/ExtensionMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.LINQ
+{
+    /// <summary>
+    /// Extension methods allow us to add methods to existing types without creating a new derived type, recompiling, or otherwise modifying the original type. 
+    /// </summary>
+    class ExtensionMethod
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Welcome to Dotnet Tutorials";
+
+            int wordCount = sentence.GetWordCount();
+            Console.WriteLine($"Count : {wordCount}");
+            Console.ReadKey();
+        }
+    }
+
+    public static class ExtensionHelper
+    {
+        public static int GetWordCount(this string str)
+        {
+            if (!String.IsNullOrEmpty(str))
+                return str.Split(' ').Length;
+            return 0;
+        }
+    }
+}
