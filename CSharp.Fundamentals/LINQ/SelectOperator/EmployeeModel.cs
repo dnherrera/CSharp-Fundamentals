8f2a433f2b9ea26a4a3d1ad606b1314a9f99d920
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-02-10 21:41:32 +0800

    Select Query

---
+++ CSharp.Fundamentals/LINQ/SelectOperator/EmployeeModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp.Fundamentals.LINQ.SelectOperator
+{
+    public class EmployeeModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+        public static List<EmployeeModel> GetEmployees()
+        {
+            List<EmployeeModel> employees = new List<EmployeeModel>
+            {
+                new EmployeeModel {ID = 101, FirstName = "Preety", LastName = "Tiwary", Salary = 60000 },
+                new EmployeeModel {ID = 102, FirstName = "Priyanka", LastName = "Dewangan", Salary = 70000 },
+                new EmployeeModel {ID = 103, FirstName = "Hina", LastName = "Sharma", Salary = 80000 },
+                new EmployeeModel {ID = 104, FirstName = "Anurag", LastName = "Mohanty", Salary = 90000 },
+                new EmployeeModel {ID = 105, FirstName = "Sambit", LastName = "Satapathy", Salary = 100000 },
+                new EmployeeModel {ID = 106, FirstName = "Sushanta", LastName = "Jena", Salary = 160000 }
+            };
+            return employees;
+        }
+    }
+}
---
+++ CSharp.Fundamentals/LINQ/SelectOperator/SelectQuery.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.Fundamentals.LINQ.SelectOperator
+{
+    class SelectQuery
+    {
+        static void Main(string[] args)
+        {
+            //Query Syntax
+            IEnumerable<EmployeeModel> selectQuery = (from emp in EmployeeModel.GetEmployees()
+                                                 select new EmployeeModel()
+                                                 {
+                                                     FirstName = emp.FirstName,
+                                                     LastName = emp.LastName,
+                                                     Salary = emp.Salary
+                                                 });
+
+            foreach (var emp in selectQuery)
+            {
+                Console.WriteLine($" Name : {emp.FirstName} {emp.LastName} Salary : {emp.Salary} ");
+            }
+
+            //Method Syntax
+            List<EmployeeModel> selectMethod = EmployeeModel.GetEmployees().
+                                          Select(emp => new EmployeeModel()
+                                          {
+                                              FirstName = emp.FirstName,
+                                              LastName = emp.LastName,
+                                              Salary = emp.Salary
+                                          }).ToList();
+
+            foreach (var emp in selectMethod)
+            {
+                Console.WriteLine($" Name : {emp.FirstName} {emp.LastName} Salary : {emp.Salary} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
