caa6ca55eec69679032d1ceed709d362134143c0
dn.hrr <dondonherrera02@gmail.com>
2022-02-13 21:46:05 +0800

    OfType Operator

---
+++ CSharp.Fundamentals/LINQ/OfTypeOperator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Fundamentals.LINQ
+{
+    /// <summary>
+    /// OfType Operator in LINQ is used to filter specific type data from a data source based on the data type we passed to this operator.
+    /// </summary>
+    class OfTypeOperator
+    {
+        static void Main(string[] args)
+        {
+            List<object> dataSource = new List<object>()
+            {
+                "Tom", "Mary", 50, "Prince", "Jack", 10, 20, 30, 40, "James"
+            };
+            //Using Method Syntax
+            var intData = dataSource.OfType<int>().Where(num => num > 30).ToList();
+            foreach (int number in intData)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            //Using Qyery Syntax
+            var stringData = (from name in dataSource
+                              where name is string && name.ToString().Length > 3
+                              select name).ToList();
+            foreach (string name in stringData)
+            {
+                Console.Write(name + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
