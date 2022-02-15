9cc957ca863cef8e1d093e97bfd38dc9a492a9e6
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-02-15 23:32:33 +0800

    Distinct Operator and IEqualityComparer

---
+++ CSharp.Fundamentals/LINQ/SetOperator/DistinctOperator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp.Fundamentals.LINQ.SetOperator
+{
+    public class DistinctOperator
+    {
+        static void Main(string[] args)
+        {
+            //Creating an instance of StudentComparer
+            StudentComparer studentComparer = new StudentComparer();
+
+            //Using Method Syntax
+            var MS = Student.GetStudents()
+                    .Distinct(studentComparer).ToList();
+
+            //Using Query Syntax
+            var QS = (from std in Student.GetStudents()
+                      select std)
+                      .Distinct(studentComparer).ToList();
+            foreach (var item in QS)
+            {
+                Console.WriteLine($"ID : {item.ID} , Name : {item.Name} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp.Fundamentals/LINQ/SetOperator/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp.Fundamentals.LINQ.SetOperator
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student {ID = 101, Name = "Preety" },
+                new Student {ID = 102, Name = "Sambit" },
+                new Student {ID = 103, Name = "Hina"},
+                new Student {ID = 104, Name = "Anurag"},
+                new Student {ID = 102, Name = "Sambit"},
+                new Student {ID = 103, Name = "Hina"},
+                new Student {ID = 101, Name = "Preety" },
+            };
+            return students;
+        }
+    }
+}
