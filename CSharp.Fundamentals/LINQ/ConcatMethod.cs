66358cd00235f53d65266d7279cbdcdfcad0e44c
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-02-19 14:20:57 +0800

    Concat Method

---
+++ CSharp.Fundamentals/LINQ/ConcatMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Fundamentals.LINQ
+{
+    /// <summary>
+    /// Linq Concat Method in C# is used to concatenate two sequences into one sequence
+    /// </summary>
+    class ConcatMethod
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequence1 = new List<int> { 1, 2, 3, 4 };
+            List<int> sequence2 = new List<int> { 2, 4, 6, 8 };
+            var result = sequence1.Concat(sequence2);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
