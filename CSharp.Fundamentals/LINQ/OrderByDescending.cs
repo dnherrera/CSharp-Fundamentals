4cf68cc0ea077fbec86c09a70e61ff622c3499fe
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-02-21 21:06:09 +0800

    Order by Descending

---
+++ CSharp.Fundamentals/LINQ/OrderByDescending.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Fundamentals.LINQ
+{
+    class OrderByDescending
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringList = new List<string>() { "Preety", "Tiwary", "Agrawal", "Priyanka", "Dewangan",
+            "Hina","Kumar","Manoj", "Rout", "James"};
+            //Using Method Syntax
+            var MS = stringList.OrderByDescending(name => name);
+            //Using Query Syntax
+            var QS = (from name in stringList
+                      orderby name descending
+                      select name).ToList();
+            foreach (var item in MS)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
