d0f904d7445e752df30f4542363f9cc34df8ab07
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-02-25 10:08:36 +0800

    Max Method

--- CSharp.Fundamentals/LINQ/Aggregators/SumMethod.cs
+++ CSharp.Fundamentals/LINQ/Aggregators/SumMethod.cs
@@ -12,11 +12,11 @@ namespace CSharp.Fundamentals.LINQ.Aggregators
         static void Main(string[] args)
         {
             //Using Method Syntax
-            var TotalSalaryMS = Employee.GetAllEmployees()
+            var TotalSalaryMS = EmpleyadoModel.GetAllEmployees()
                               .Where(emp => emp.Department == "IT")
                               .Sum(emp => emp.Salary);
             //Using Query Syntax
-            var TotalSalaryQS = (from emp in Employee.GetAllEmployees()
+            var TotalSalaryQS = (from emp in EmpleyadoModel.GetAllEmployees()
                                  where emp.Department == "IT"
                                  select emp).Sum(e => e.Salary);
 
@@ -25,26 +25,26 @@ namespace CSharp.Fundamentals.LINQ.Aggregators
         }
     }
 
-    public class Employee
+    public class EmpleyadoModel
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public int Salary { get; set; }
         public string Department { get; set; }
-        public static List<Employee> GetAllEmployees()
+        public static List<EmpleyadoModel> GetAllEmployees()
         {
-            List<Employee> listStudents = new List<Employee>()
+            List<EmpleyadoModel> listStudents = new List<EmpleyadoModel>()
             {
-                new Employee{ID= 101,Name = "Preety", Salary = 10000, Department = "IT"},
-                new Employee{ID= 102,Name = "Priyanka", Salary = 15000, Department = "Sales"},
-                new Employee{ID= 103,Name = "James", Salary = 50000, Department = "Sales"},
-                new Employee{ID= 104,Name = "Hina", Salary = 20000, Department = "IT"},
-                new Employee{ID= 105,Name = "Anurag", Salary = 30000, Department = "IT"},
-                new Employee{ID= 106,Name = "Sara", Salary = 25000, Department = "IT"},
-                new Employee{ID= 107,Name = "Pranaya", Salary = 35000, Department = "IT"},
-                new Employee{ID= 108,Name = "Manoj", Salary = 11000, Department = "Sales"},
-                new Employee{ID= 109,Name = "Sam", Salary = 45000, Department = "Sales"},
-                new Employee{ID= 110,Name = "Saurav", Salary = 25000, Department = "Sales"}
+                new EmpleyadoModel{ID= 101,Name = "Preety", Salary = 10000, Department = "IT"},
+                new EmpleyadoModel{ID= 102,Name = "Priyanka", Salary = 15000, Department = "Sales"},
+                new EmpleyadoModel{ID= 103,Name = "James", Salary = 50000, Department = "Sales"},
+                new EmpleyadoModel{ID= 104,Name = "Hina", Salary = 20000, Department = "IT"},
+                new EmpleyadoModel{ID= 105,Name = "Anurag", Salary = 30000, Department = "IT"},
+                new EmpleyadoModel{ID= 106,Name = "Sara", Salary = 25000, Department = "IT"},
+                new EmpleyadoModel{ID= 107,Name = "Pranaya", Salary = 35000, Department = "IT"},
+                new EmpleyadoModel{ID= 108,Name = "Manoj", Salary = 11000, Department = "Sales"},
+                new EmpleyadoModel{ID= 109,Name = "Sam", Salary = 45000, Department = "Sales"},
+                new EmpleyadoModel{ID= 110,Name = "Saurav", Salary = 25000, Department = "Sales"}
             };
             return listStudents;
         }
