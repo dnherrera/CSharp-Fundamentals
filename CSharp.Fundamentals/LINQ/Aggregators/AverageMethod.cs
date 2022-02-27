19b96d9edc21fbbb8318132f1dfd1298a59182fb
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-02-27 11:43:54 +0800

    Average Method

---
+++ CSharp.Fundamentals/LINQ/Aggregators/AverageMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp.Fundamentals.LINQ.Aggregators
+{
+    class AverageMethod
+    {
+        static void Main(string[] args)
+        {
+            //Using Method Syntax
+            var MSAverageSalary = EmpleyadoModel.GetAllEmployees()
+                                 .Where(emp => emp.Department == "IT")
+                                 .Average(emp => emp.Salary);
+            //Using Query Syntax
+            var QSAverageSalary = (from emp in EmpleyadoModel.GetAllEmployees()
+                                   where emp.Department == "IT"
+                                   select emp).Average(e => e.Salary);
+            Console.WriteLine("IT Department Average Salary = " + MSAverageSalary);
+            Console.ReadKey();
+        }
+    }
+}
