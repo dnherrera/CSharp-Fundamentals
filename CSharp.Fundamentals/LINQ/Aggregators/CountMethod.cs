296430c46ae5005b57872e8dd41d8812ef80e39d
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-02-28 23:48:05 +0800

    Count Method

---
+++ CSharp.Fundamentals/LINQ/Aggregators/CountMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp.Fundamentals.LINQ.Aggregators
+{
+    class CountMethod
+    {
+        static void Main(string[] args)
+        {
+            int[] intNumbers = new int[] { 60, 80, 50, 90, 10, 30, 70, 40, 20, 100 };
+            //Using Method Syntax
+            int MSCount = intNumbers.Where(num => num > 40).Count();
+            //Using Query Syntax
+            var QSCount = (from num in intNumbers
+                           where num > 40
+                           select num).Count();
+            Console.WriteLine("No of Elements = " + MSCount);
+            Console.ReadKey();
+        }
+    }
+}
