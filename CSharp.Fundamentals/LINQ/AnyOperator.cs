50562240b51b8cdb48e2f15e56a4e49ae48c67bc
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-03-07 21:53:03 +0800

    Any Operator

---
+++ CSharp.Fundamentals/LINQ/AnyOperator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp.Fundamentals.LINQ
+{
+    /// <summary>
+    /// C# Linq Any Operator is used to check whether at least one of the elements of a data source satisfies a given condition or not. 
+    /// </summary>
+    class AnyOperator
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = { "James", "Sachin", "Sourav", "Pam", "Sara" };
+
+            //Method Syntax
+            var ResultMS = stringArray.Any(name => name.Length > 5);
+
+            //Query Syntax
+            var ResultQS = (from name in stringArray
+                            select name).Any(name => name.Length > 5);
+
+            Console.WriteLine("Is Any name with length  greater than 5 Characters : " + ResultMS);
+            Console.ReadKey();
+        }
+    }
+}
