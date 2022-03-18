c24d390da32c7ae373a317927cf2c4fc888a2eae
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2022-03-18 21:55:03 +0800

    Linq Join with Multiple Data Sources in C#

---
+++ CSharp.Fundamentals/LINQ/Joins/MultipleJoins.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp.Fundamentals.LINQ.Joins
+{
+    class MultipleJoins
+    {
+        static void Main(string[] args)
+        {
+            var JoinMultipleDSUsingQS = (//Data Source1
+                                         from emp in UserModel.GetAllEmployees()
+                                             //Joining with Address Data Source (Data Source2)
+                                         join adrs in AddressModel.GetAllAddresses()
+                                         on emp.AddressId equals adrs.ID
+                                         //Joining with Department Data Source (Data Source3)
+                                         join dept in DepartmentModel.GetAllDepartments()
+                                         on emp.DepartmentId equals dept.ID
+                                         //Projecting the result set
+                                         select new
+                                         {
+                                             ID = emp.ID,
+                                             EmployeeName = emp.Name,
+                                             DepartmentName = dept.Name,
+                                             AddressLine = adrs.AddressLine
+                                         }).ToList();
+            foreach (var employee in JoinMultipleDSUsingQS)
+            {
+                Console.WriteLine($"ID = {employee.ID}, Name = {employee.EmployeeName}, Department = {employee.DepartmentName}, Addres = {employee.AddressLine}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- CSharp.Fundamentals/LINQ/Joins/UserModel.cs
+++ CSharp.Fundamentals/LINQ/Joins/UserModel.cs
@@ -7,21 +7,22 @@ namespace CSharp.Fundamentals.LINQ.Joins
         public int ID { get; set; }
         public string Name { get; set; }
         public int AddressId { get; set; }
+        public int DepartmentId { get; set; }
         public static List<UserModel> GetAllEmployees()
         {
             return new List<UserModel>()
             {
-                new UserModel { ID = 1, Name = "Preety", AddressId = 1 },
-                new UserModel { ID = 2, Name = "Priyanka", AddressId = 2 },
-                new UserModel { ID = 3, Name = "Anurag", AddressId = 3 },
-                new UserModel { ID = 4, Name = "Pranaya", AddressId = 4 },
-                new UserModel { ID = 5, Name = "Hina", AddressId = 5 },
-                new UserModel { ID = 6, Name = "Sambit", AddressId = 6 },
-                new UserModel { ID = 7, Name = "Happy", AddressId = 7},
-                new UserModel { ID = 8, Name = "Tarun", AddressId = 8 },
-                new UserModel { ID = 9, Name = "Santosh", AddressId = 9 },
-                new UserModel { ID = 10, Name = "Raja", AddressId = 10},
-                new UserModel { ID = 11, Name = "Sudhanshu", AddressId = 11}
+                new UserModel { ID = 1, Name = "Preety", AddressId = 1, DepartmentId = 10    },
+                new UserModel { ID = 2, Name = "Priyanka", AddressId = 2, DepartmentId =20   },
+                new UserModel { ID = 3, Name = "Anurag", AddressId = 3, DepartmentId = 30    },
+                new UserModel { ID = 4, Name = "Pranaya", AddressId = 4, DepartmentId = 0    },
+                new UserModel { ID = 5, Name = "Hina", AddressId = 5, DepartmentId = 0       },
+                new UserModel { ID = 6, Name = "Sambit", AddressId = 6, DepartmentId = 0     },
+                new UserModel { ID = 7, Name = "Happy", AddressId = 7, DepartmentId = 0      },
+                new UserModel { ID = 8, Name = "Tarun", AddressId = 8, DepartmentId = 0      },
+                new UserModel { ID = 9, Name = "Santosh", AddressId = 9, DepartmentId = 10   },
+                new UserModel { ID = 10, Name = "Raja", AddressId = 10, DepartmentId = 20    },
+                new UserModel { ID = 11, Name = "Ramesh", AddressId = 11, DepartmentId = 30  }
             };
         }
     }
